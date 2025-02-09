6bddc7d2936fed55a37d928693d1171adbcd0d59
Yaman Sharma <iamymnsha@gmail.com>
2025-02-09 13:36:53 +0530

    Simple Redux Flow Implementation
    It teaches the basic implementation of redux with state, reducers, action, action type, store, subscribe and dispatch. How everything works and connects together.

--- script.js
+++ script.js
@@ -1,27 +1,77 @@
-let state = {
-    count: 0,
-    name: "jacob"
-}
+import { createStore } from "https://cdn.skypack.dev/redux";
 
-let prevState = state;
+//STORE'S STATE i.e. an account state in a bank
 
-console.log(prevState, "prevState")
+const accountState = {
+  name: "Yaman Sharma",
+  age: 28,
+  accountNumber: "123456789",
+  balance: 4000,
+};
 
-function increment () {
-    //mutating state - prevState will be same as current state
-    // state.count = state.count + 1;
-    //not mutating state - prevState will not be same as current state
-    //we spread state to preserve the other state in the object
-    //we are providing a new state and hence during comparison it will not match the address and update
-    state = {
+//ACTION TYPES
+
+const CHECK_BALANCE = "ACCOUNT/CHECK_BALANCE";
+const INCREMENT = "ACCOUNT/INCREMENT";
+const DECREMENT = "ACCOUNT/DECREMENT";
+const INCREMENTBY = "ACCOUNT/INCREMENTBY";
+const DECREMENTBY = "ACCOUNT/DECREMENTBY";
+
+//REDUCER/CASHIER - Acocunt Reducer/Account Updater
+// It's like a cashier in the bank, which takes ur initial state (aka bank statement) and an action to perform
+// It checks the action type in action and makes a new statement (but it doesnt directly update state as it will be critical) and provides to store (ie. bank central system to update)
+
+let accountReducer = (state = accountState, action) => {
+  switch (action.type) {
+    case CHECK_BALANCE:
+      return state;
+    case INCREMENT:
+      return {
         ...state,
-        count: state.count + 1
-    }
-}
+        balance: state.balance + 100,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        balance: state.balance - 100,
+      };
+    case INCREMENTBY:
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case DECREMENTBY:
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    default:
+        return state;
+  }
+};
 
-increment();
-console.log(state)
-increment();
-console.log(state)
+// We call the reducer with state and action payload under manually
+
+// console.log(accountReducer(accountState, {type: CHECK_BALANCE}));
+// console.log(accountReducer(accountState, {type: INCREMENT}));
+// console.log(accountReducer(accountState, {type: DECREMENT}));
+// console.log(accountReducer(accountState, {type: INCREMENT_BY, payload: 10}));
+// console.log(accountReducer(accountState, {type: DECREMENT_BY, payload: 10}));
+
+//In case of redux, we pass the reducer to createStore function
+const store = createStore(accountReducer);
 
 
+//SUBSCRIBE is like a notification system which notifies if anything in the state changes i.e. if anything in bank acocunt changes
+store.subscribe(()=>{
+    //listen for changes in state and console logs
+    console.log(store.getState(), "state")
+})
+
+//with getState() we can ask the current state
+console.log(store.getState(), "store")
+
+//DISPATCH - Telling the reducer to perform an action i.e. cashier do this
+store.dispatch({type: DECREMENT});
+store.dispatch({type: INCREMENT});
+store.dispatch({type: DECREMENTBY, payload:100});
